5d3f35d2d8a1a4ccb091219416662c8e02051ddb
hosizoraru <hosizoraru@gmail.com>
2023-05-18 04:47:57 +0800

    修正一处String拼写错误

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/MonetColor.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/MonetColor.kt
@@ -7,12 +7,12 @@ import star.sky.voyager.utils.key.XSPUtils
 import star.sky.voyager.utils.key.hasEnable
 
 object MonetColor : ResourcesHookRegister() {
-    override fun init() = hasEnable("monoet_color") {
+    override fun init() = hasEnable("monet_color") {
         val monet = "system_accent1_100"
         val monoColorId = Resources.getSystem().getIdentifier(monet, "color", "android")
         var monoColor = Resources.getSystem().getColor(monoColorId)
         hasEnable("use_edit_color") {
-            monoColor = Color.parseColor(XSPUtils.getString("your_color","#0d84ff"))
+            monoColor = Color.parseColor(XSPUtils.getString("your_color", "#0d84ff"))
         }
         getInitPackageResourcesParam().res.setReplacement(
             "com.miui.home",
