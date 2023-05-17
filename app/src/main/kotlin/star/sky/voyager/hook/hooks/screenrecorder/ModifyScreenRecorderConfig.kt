a5bf0f1b14188f18139cced472230cfd2efb5761
hosizoraru <hosizoraru@gmail.com>
2023-05-18 00:55:58 +0800

    优化 修改码率与帧率范围

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/screenrecorder/ModifyScreenRecorderConfig.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/screenrecorder/ModifyScreenRecorderConfig.kt
@@ -13,11 +13,8 @@ object ModifyScreenRecorderConfig : HookRegister() {
         loadDexKit()
         dexKitBridge.findMethodUsingString {
             usingString = "Error when set frame value, maxValue = "
-        }.map {
-            it.getMethodInstance(EzXHelper.safeClassLoader)
-        }.firstOrNull {
-            it.parameterCount == 2 && it.parameterTypes[0] == Int::class.java && it.parameterTypes[1] == Int::class.java
-        }?.createHook {
+            methodParamTypes = arrayOf("I", "I")
+        }.firstOrNull()?.getMethodInstance(EzXHelper.safeClassLoader)?.createHook {
             before { param ->
                 param.args[0] = 3600
                 param.args[1] = 1
@@ -25,12 +22,11 @@ object ModifyScreenRecorderConfig : HookRegister() {
                     field.also {
                         it.isAccessible = true
                     }.let { fieldAccessible ->
-                        fieldAccessible.isFinal &&
-                                fieldAccessible.get(null).let {
-                                    kotlin.runCatching {
-                                        (it as IntArray).contentEquals(intArrayOf(15, 24, 30, 48, 60, 90))
-                                    }.getOrDefault(false)
-                                }
+                        fieldAccessible.isFinal && fieldAccessible.get(null).let {
+                            kotlin.runCatching {
+                                (it as IntArray).contentEquals(intArrayOf(15, 24, 30, 48, 60, 90))
+                            }.getOrDefault(false)
+                        }
                     }
                 }?.set(null, intArrayOf(15, 24, 30, 48, 60, 90, 120, 144))
             }
@@ -49,12 +45,24 @@ object ModifyScreenRecorderConfig : HookRegister() {
                     field.also {
                         it.isAccessible = true
                     }.let { fieldAccessible ->
-                        fieldAccessible.isFinal &&
-                                fieldAccessible.get(null).let {
-                                    kotlin.runCatching {
-                                        (it as IntArray).contentEquals(intArrayOf(200, 100, 50, 32, 24, 16, 8, 6, 4, 1))
-                                    }.getOrDefault(false)
-                                }
+                        fieldAccessible.isFinal && fieldAccessible.get(null).let {
+                            kotlin.runCatching {
+                                (it as IntArray).contentEquals(
+                                    intArrayOf(
+                                        200,
+                                        100,
+                                        50,
+                                        32,
+                                        24,
+                                        16,
+                                        8,
+                                        6,
+                                        4,
+                                        1
+                                    )
+                                )
+                            }.getOrDefault(false)
+                        }
                     }
                 }?.set(null, intArrayOf(1200, 1000, 800, 600, 400, 200, 100, 50, 32, 24, 16))
             }
