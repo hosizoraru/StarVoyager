4e2ee75065832668359c75688709f17b713704bd
hosizoraru <hosizoraru@gmail.com>
2023-05-23 01:41:14 +0800

    使用Ezx继续重构部分代码

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/AddFreeformShortcut.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/AddFreeformShortcut.kt
@@ -6,9 +6,14 @@ import android.app.ActivityOptions
 import android.content.ComponentName
 import android.content.Intent
 import android.view.View
-import com.github.kyuubiran.ezxhelper.ClassUtils
+import com.github.kyuubiran.ezxhelper.ClassUtils.getStaticObjectOrNullAs
+import com.github.kyuubiran.ezxhelper.ClassUtils.invokeStaticMethodBestMatch
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.github.kyuubiran.ezxhelper.EzXHelper
+import com.github.kyuubiran.ezxhelper.EzXHelper.hostPackageName
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import de.robv.android.xposed.XposedHelpers
 import star.sky.voyager.R
@@ -19,23 +24,24 @@ object AddFreeformShortcut : HookRegister() {
     @SuppressLint("DiscouragedApi")
     override fun init() = hasEnable("add_freeform_shortcut") {
         val clazzSystemShortcutMenuItem =
-            ClassUtils.loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem")
+            loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem")
 
         Activity::class.java.methodFinder().filterByName("onCreate").toList().createHooks {
             after {
                 EzXHelper.initAppContext(it.thisObject as Activity)
             }
         }
-        ClassUtils.loadClass("com.miui.home.launcher.util.ViewDarkModeHelper").methodFinder()
+        loadClass("com.miui.home.launcher.util.ViewDarkModeHelper").methodFinder()
             .filterByName("onConfigurationChanged").toList().createHooks {
                 after {
-                    ClassUtils.invokeStaticMethodBestMatch(
+                    invokeStaticMethodBestMatch(
                         clazzSystemShortcutMenuItem,
                         "createAllSystemShortcutMenuItems"
                     )
                 }
             }
-        ClassUtils.loadClass("com.miui.home.launcher.shortcuts.ShortcutMenuItem").methodFinder().filterByName("getShortTitle")
+        loadClass("com.miui.home.launcher.shortcuts.ShortcutMenuItem").methodFinder()
+            .filterByName("getShortTitle")
             .toList().createHooks {
                 after { param ->
                     if (param.result.equals("应用信息")) {
@@ -43,11 +49,11 @@ object AddFreeformShortcut : HookRegister() {
                     }
                 }
             }
-        ClassUtils.loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem\$AppDetailsShortcutMenuItem")
+        loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem\$AppDetailsShortcutMenuItem")
             .methodFinder()
             .filterByName("getOnClickListener").toList().createHooks {
                 before { param ->
-                    when (XposedHelpers.callMethod(param.thisObject, "getShortTitle")) {
+                    when (invokeMethodBestMatch(param.thisObject, "getShortTitle")) {
                         EzXHelper.moduleRes.getString(R.string.freeform) -> {
                             param.result = View.OnClickListener { view ->
                                 val context = view.context
@@ -61,9 +67,10 @@ object AddFreeformShortcut : HookRegister() {
                                     component = componentName
                                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 }
-                                XposedHelpers.callStaticMethod(
-                                    ClassUtils.loadClass("com.miui.launcher.utils.ActivityUtilsCompat"),
+                                invokeStaticMethodBestMatch(
+                                    loadClass("com.miui.launcher.utils.ActivityUtilsCompat"),
                                     "makeFreeformActivityOptions",
+                                    null,
                                     context,
                                     componentName.packageName
                                 )?.let {
@@ -77,13 +84,13 @@ object AddFreeformShortcut : HookRegister() {
                     }
                 }
             }
-        ClassUtils.loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenu").methodFinder()
+        loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenu").methodFinder()
             .filterByName("getMaxShortcutItemCount").toList().createHooks {
                 after { param ->
                     param.result = 5
                 }
             }
-        ClassUtils.loadClass("com.miui.home.launcher.shortcuts.AppShortcutMenu").methodFinder()
+        loadClass("com.miui.home.launcher.shortcuts.AppShortcutMenu").methodFinder()
             .filterByName("getMaxShortcutItemCount").toList().createHooks {
                 after { param ->
                     param.result = 5
@@ -92,28 +99,28 @@ object AddFreeformShortcut : HookRegister() {
         clazzSystemShortcutMenuItem.methodFinder()
             .filterByName("createAllSystemShortcutMenuItems").toList().createHooks {
                 after {
-                    @Suppress("UNCHECKED_CAST")
-                    val mAllSystemShortcutMenuItems =
-                        XposedHelpers.getStaticObjectField(
-                            clazzSystemShortcutMenuItem,
-                            "sAllSystemShortcutMenuItems"
-                        ) as List<Any>
+                    val mAllSystemShortcutMenuItems = getStaticObjectOrNullAs<List<Any>>(
+                        clazzSystemShortcutMenuItem,
+                        "sAllSystemShortcutMenuItems"
+                    )!!
                     val mSmallWindowInstance =
-                        ClassUtils.loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem\$AppDetailsShortcutMenuItem")
+                        loadClass("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem\$AppDetailsShortcutMenuItem")
                             .newInstance()
                             .apply {
-                                XposedHelpers.callMethod(
+                                invokeMethodBestMatch(
                                     this,
                                     "setShortTitle",
+                                    null,
                                     EzXHelper.moduleRes.getString(R.string.freeform)
                                 )
-                                XposedHelpers.callMethod(
-                                    this, "setIconDrawable", EzXHelper.appContext.let {
+                                invokeMethodBestMatch(
+                                    this,
+                                    "setIconDrawable",
+                                    null,
+                                    EzXHelper.appContext.let {
                                         it.getDrawable(
                                             it.resources.getIdentifier(
-                                                "ic_task_small_window",
-                                                "drawable",
-                                                EzXHelper.hostPackageName
+                                                "ic_task_small_window", "drawable", hostPackageName
                                             )
                                         )
                                     }
@@ -124,7 +131,7 @@ object AddFreeformShortcut : HookRegister() {
                         add(mSmallWindowInstance)
                         addAll(mAllSystemShortcutMenuItems)
                     }
-                    XposedHelpers.setStaticObjectField(
+                    setStaticObject(
                         clazzSystemShortcutMenuItem,
                         "sAllSystemShortcutMenuItems",
                         sAllSystemShortcutMenuItems
--- app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/NotificationSettingsNoWhiteList.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/NotificationSettingsNoWhiteList.kt
@@ -1,17 +1,16 @@
 package star.sky.voyager.hook.hooks.systemui
 
-import com.github.kyuubiran.ezxhelper.ClassHelper.Companion.classHelper
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
-import de.robv.android.xposed.XposedHelpers
+import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object NotificationSettingsNoWhiteList : HookRegister() {
     override fun init() = hasEnable("notification_settings_no_white_list") {
-        if (loadClass("miui.os.Build").classHelper()
-                .getStaticObjectOrNullAs<Boolean>("IS_INTERNATIONAL_BUILD")!!
+        if (loadClass("miui.os.Build").getField("IS_INTERNATIONAL_BUILD")
+                .getBoolean(null)
         ) return@hasEnable
-        XposedHelpers.setStaticObjectField(
+        setStaticObject(
             loadClass("com.android.systemui.statusbar.notification.NotificationSettingsManager"),
             "USE_WHITE_LISTS",
             false
