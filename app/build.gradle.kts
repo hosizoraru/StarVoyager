0cfd20c5f0c7ea383f720746f3d09a21efeff1e6
hosizoraru <hosizoraru@gmail.com>
2023-05-24 06:15:20 +0800

    update version to 1.85(11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "star.sky.voyager"
         minSdk = 33
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.8"
+        versionCode = 11
+        versionName = "1.85"
         buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
     }
 
