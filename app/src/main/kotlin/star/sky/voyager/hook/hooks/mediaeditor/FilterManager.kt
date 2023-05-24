5165ba1806da0fd9e3d5b0830958c519be700adc
hosizoraru <hosizoraru@gmail.com>
2023-05-25 05:03:53 +0800

    DexKit掉了大师滤镜的hook

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/mediaeditor/FilterManager.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/mediaeditor/FilterManager.kt
@@ -1,21 +1,25 @@
 package star.sky.voyager.hook.hooks.mediaeditor
 
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.EzXHelper.classLoader
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.FieldFinder.`-Static`.fieldFinder
-import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
+import star.sky.voyager.utils.yife.DexKit.dexKitBridge
+import star.sky.voyager.utils.yife.DexKit.loadDexKit
 
 object FilterManager : HookRegister() {
     override fun init() = hasEnable("filter_manager") {
-        loadClass("com.miui.gallery.editor.photo.core.imports.filter.FilterManager").methodFinder().first {
-            name == "g"
-        }.createHook {
+        loadDexKit()
+        dexKitBridge.findMethodUsingString {
+            usingString = "wayne"
+            methodReturnType = "Ljava/util/List;"
+        }.single().getMethodInstance(classLoader).createHook {
             before {
                 loadClass("android.os.Build").fieldFinder().first {
                     type == String::class.java && name == "DEVICE"
-                }.set(null,"wayne")
+                }.set(null, "wayne")
             }
         }
     }
