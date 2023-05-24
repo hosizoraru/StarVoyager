0163e57306995152d39a7e61f57ff43c7f9df053
hosizoraru <hosizoraru@gmail.com>
2023-05-25 01:25:50 +0800

    修复 去除触控蓝牙笔限制

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/maxmipad/MiuiStylusDeviceListener.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/maxmipad/MiuiStylusDeviceListener.kt
@@ -1,42 +1,41 @@
 package star.sky.voyager.hook.hooks.maxmipad
 
-import com.github.kyuubiran.ezxhelper.ClassUtils.invokeStaticMethodBestMatch
+import android.os.Build
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
-import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
+import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+import star.sky.voyager.utils.api.callMethod
 import star.sky.voyager.utils.init.HookRegister
+import star.sky.voyager.utils.key.XSPUtils.getString
 import star.sky.voyager.utils.key.hasEnable
-import star.sky.voyager.utils.yife.XSharedPreferences
 
 object MiuiStylusDeviceListener : HookRegister() {
     override fun init() = hasEnable("remove_stylus_bluetooth_restriction") {
-        val driverVersion =
-            XSharedPreferences.getString("remove_stylus_bluetooth_restriction_driver_version", "2")
-                .toInt()
-        val MiuiStylusDeviceListenerClass =
-            loadClass("com.miui.server.input.stylus.MiuiStylusDeviceListener")
-        MiuiStylusDeviceListenerClass.constructors.createHooks {
+        val isNew = Build.VERSION.SDK_INT == Build.VERSION_CODES.TIRAMISU
+        val clazzMiuiStylusDeviceListener =
+            if (isNew) loadClass("com.miui.server.input.stylus.MiuiStylusDeviceListener")
+            else loadClass("com.miui.server.stylus.MiuiStylusDeviceListener") // 目前是 A12
+        clazzMiuiStylusDeviceListener.declaredConstructors.createHooks {
             after {
-                invokeStaticMethodBestMatch(
-                    loadClass("miui.util.ITouchFeature"),
-                    "getInstance"
-                )?.let {
-                    invokeMethodBestMatch(it, "setTouchMode", null, 0, 20, 0x10 or driverVersion)
-                }
+                setTouchModeStylusEnable(isNew)
             }
         }
-        MiuiStylusDeviceListenerClass.methodFinder().filter {
-            true
-        }.toList().createHooks {
-            replace {
-                invokeStaticMethodBestMatch(
-                    loadClass("miui.util.ITouchFeature"),
-                    "getInstance"
-                )?.let {
-                    invokeMethodBestMatch(it, "setTouchMode", null, 0, 20, 0x10 or driverVersion)
-                }
+
+        clazzMiuiStylusDeviceListener.declaredMethods.createHooks {
+            after {
+                setTouchModeStylusEnable(isNew)
             }
         }
+    }
+
+    private fun setTouchModeStylusEnable(isNew: Boolean) {
+        val driverVersion =
+            getString("remove_stylus_bluetooth_restriction_driver_version", "2")
+                ?.toInt()
+        val flag = if (isNew) (0x10 or driverVersion!!) else 1
+        loadClass("miui.util.ITouchFeature").methodFinder().filterByName("getInstance").first()
+            .invoke(null)?.objectHelper()
+            ?.callMethod("setTouchMode", 0, 20, flag)
     }
 }
