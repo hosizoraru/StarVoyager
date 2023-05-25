1b2a42ede880172896881ab4610f2b3e2baa436b
hosizoraru <hosizoraru@gmail.com>
2023-05-26 05:21:13 +0800

    小米互传从磁贴开启时不再toast提示10min自动关闭

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/mishare/NoAutoTurnOff.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/mishare/NoAutoTurnOff.kt
@@ -1,7 +1,9 @@
 package star.sky.voyager.hook.hooks.mishare
 
+import android.content.Context
 import com.github.kyuubiran.ezxhelper.ClassLoaderProvider.classLoader
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import io.luckypray.dexkit.enums.MatchType
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
@@ -21,6 +23,25 @@ object NoAutoTurnOff : HookRegister() {
                 before {
                     it.result = null
                 }
+            }
+        }
+
+        dexKitBridge.batchFindClassesUsingStrings {
+            addQuery("qwq", listOf("null context", "cta_agree"))
+            matchType = MatchType.FULL
+        }.forEach { (_, classes) ->
+            classes.map {
+                it.getClassInstance(classLoader).methodFinder()
+                    .filterByReturnType(Boolean::class.java)
+                    .filterByParamCount(2)
+                    .filterByParamTypes(Context::class.java, String::class.java)
+                    .toList().createHooks {
+                        before { param ->
+                            if (param.args[1].equals("security_agree")) {
+                                param.result = false
+                            }
+                        }
+                    }
             }
         }
     }
