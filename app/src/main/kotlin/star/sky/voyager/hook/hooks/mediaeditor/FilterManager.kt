706c7ac08d556759d76127e96c1eedec287435dd
hosizoraru <hosizoraru@gmail.com>
2023-06-01 01:28:46 +0800

    修正大师滤镜hook的副作用并且使用更稳定的写法

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/mediaeditor/FilterManager.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/mediaeditor/FilterManager.kt
@@ -1,9 +1,12 @@
 package star.sky.voyager.hook.hooks.mediaeditor
 
+import android.os.Build
+import android.os.Bundle
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
-import com.github.kyuubiran.ezxhelper.EzXHelper.classLoader
+import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
+import com.github.kyuubiran.ezxhelper.EzXHelper.safeClassLoader
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.finders.FieldFinder.`-Static`.fieldFinder
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder
 import io.luckypray.dexkit.enums.MatchType
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
@@ -11,26 +14,24 @@ import star.sky.voyager.utils.yife.DexKit.dexKitBridge
 import star.sky.voyager.utils.yife.DexKit.loadDexKit
 
 object FilterManager : HookRegister() {
+    private lateinit var device: String
     override fun init() = hasEnable("filter_manager") {
         loadDexKit()
-        try {
-            setWayne("Ljava/util/List;")
-        } catch (_: Throwable) {
-            setWayne("void", arrayOf("android.os.Bundle"))
-        }
-    }
-
-    private fun setWayne(methodReturnType: String = "", methodParamTypes: Array<String>? = null) {
-        dexKitBridge.findMethodUsingString {
+        val methods = dexKitBridge.findMethodUsingString {
             usingString = "wayne"
-            this.methodReturnType = methodReturnType
-            this.methodParamTypes = methodParamTypes
             matchType = MatchType.FULL
-        }.single().getMethodInstance(classLoader).createHook {
+        }.filter { it.isMethod }.map { it.getMethodInstance(safeClassLoader) }.toTypedArray()
+        MethodFinder.fromArray(methods).first {
+            returnType == List::class.java || (parameterCount == 1 && parameterTypes[0] == Bundle::class.java)
+        }.createHook {
             before {
-                loadClass("android.os.Build").fieldFinder().first {
-                    type == String::class.java && name == "DEVICE"
-                }.set(null, "wayne")
+                if (!this@FilterManager::device.isInitialized) {
+                    device = Build.DEVICE
+                }
+                setStaticObject(loadClass("android.os.Build"), "DEVICE", "wayne")
+            }
+            after {
+                setStaticObject(loadClass("android.os.Build"), "DEVICE", device)
             }
         }
     }
