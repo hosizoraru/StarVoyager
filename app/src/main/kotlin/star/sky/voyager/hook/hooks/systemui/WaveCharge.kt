a4635262048fe1b56839973e474106a126e56c40
hosizoraru <hosizoraru@gmail.com>
2023-06-03 06:40:37 +0800

    优化 启用alpha充电动画

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/WaveCharge.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/WaveCharge.kt
@@ -2,41 +2,30 @@ package star.sky.voyager.hook.hooks.systemui
 
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
-import star.sky.voyager.utils.api.setObjectField
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object WaveCharge : HookRegister() {
     override fun init() = hasEnable("enable_wave_charge_animation") {
-        loadClass("com.android.keyguard.charge.ChargeUtils").methodFinder().first {
-            name == "supportWaveChargeAnimation"
-        }.createHook {
-            after {
-                val ex = Throwable()
-                val stackElement = ex.stackTrace
-                var mResult = false
-                val classTrue = setOf(
-                    "com.android.keyguard.charge.ChargeUtils",
-                    "com.android.keyguard.charge.container.MiuiChargeContainerView"
-                )
-                for (i in stackElement.indices) {
-                    when (stackElement[i].className) {
-                        in classTrue -> {
-                            mResult = true
-                            break
-                        }
-                    }
+        loadClass("com.android.keyguard.charge.ChargeUtils").methodFinder()
+            .filterByName("supportWaveChargeAnimation")
+            .first().createHook {
+                after { param ->
+                    val clazzTrue = setOf(
+                        "com.android.keyguard.charge.ChargeUtils",
+                        "com.android.keyguard.charge.container.MiuiChargeContainerView"
+                    )
+                    param.result = Throwable().stackTrace.any { it.className in clazzTrue }
                 }
-                it.result = mResult
             }
-        }
-        loadClass("com.android.keyguard.charge.wave.WaveView").methodFinder().first {
-            name == "updateWaveHeight"
-        }.createHook {
-            after {
-                it.thisObject.setObjectField("mWaveXOffset", 0)
+        loadClass("com.android.keyguard.charge.wave.WaveView").methodFinder()
+            .filterByName("updateWaveHeight").first()
+            .createHook {
+                after {
+                    it.thisObject.objectHelper().setObject("mWaveXOffset", 0)
+                }
             }
-        }
     }
 }
