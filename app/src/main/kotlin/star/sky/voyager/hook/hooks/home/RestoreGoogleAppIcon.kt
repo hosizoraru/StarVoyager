65c0ee67341b12339bbcba5623ff66f2c69b33b2
hosizoraru <hosizoraru@gmail.com>
2023-06-03 06:42:22 +0800

    修复 恢复 Google 图标

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/RestoreGoogleAppIcon.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/RestoreGoogleAppIcon.kt
@@ -3,21 +3,22 @@ package star.sky.voyager.hook.hooks.home
 import android.content.ComponentName
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
-import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
+import com.github.kyuubiran.ezxhelper.ObjectUtils.getObjectOrNullAs
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object RestoreGoogleAppIcon : HookRegister() {
     override fun init() = hasEnable("restore_google_app_icon") {
-        loadClass("com.miui.home.launcher.AppFilter").constructors.createHooks {
+        loadClass("com.miui.home.launcher.AppFilter").declaredConstructors.createHooks {
             after { param ->
-                param.thisObject.objectHelper {
-                    getObjectOrNullAs<HashSet<ComponentName>>("mSkippedItems")?.removeIf {
-                        it.packageName in listOf(
-                            "com.google.android.googlequicksearchbox",
-                            "com.google.android.gms"
-                        )
-                    }
+                getObjectOrNullAs<HashSet<ComponentName>>(
+                    param.thisObject,
+                    "mSkippedItems"
+                )!!.removeIf {
+                    it.packageName in setOf(
+                        "com.google.android.googlequicksearchbox",
+                        "com.google.android.gms"
+                    )
                 }
             }
         }
