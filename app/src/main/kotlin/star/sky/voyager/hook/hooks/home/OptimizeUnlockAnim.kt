424e833d35607d7c2985ecca3bb043445651eb2c
hosizoraru <hosizoraru@gmail.com>
2023-06-06 19:23:55 +0800

    优化解锁动画适配pad

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/OptimizeUnlockAnim.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/OptimizeUnlockAnim.kt
@@ -3,15 +3,22 @@ package star.sky.voyager.hook.hooks.home
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+import star.sky.voyager.utils.api.isPad
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object OptimizeUnlockAnim : HookRegister() {
     override fun init() = hasEnable("optimize_unlock_anim") {
-        loadClass("com.miui.home.launcher.compat.UserPresentAnimationCompatV12Phone").methodFinder()
+        loadClass(
+            when {
+                !isPad() -> "com.miui.home.launcher.compat.UserPresentAnimationCompatV12Phone"
+                else -> "com.miui.home.launcher.compat.UserPresentAnimationCompatV12Spring"
+            }
+        ).methodFinder()
             .filterByName("getSpringAnimator")
             .filterByParamCount(6)
-            .first().createHook {
+            .first()
+            .createHook {
                 before {
                     it.args[4] = 0.5f
                     it.args[5] = 0.5f
