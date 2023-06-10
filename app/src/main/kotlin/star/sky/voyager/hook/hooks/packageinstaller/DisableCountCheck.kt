ad7eb86e961e3fe41f2d959ab4d0af11e4f66faf
hosizoraru <hosizoraru@gmail.com>
2023-06-11 07:16:17 +0800

    减少一些log

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/DisableCountCheck.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/DisableCountCheck.kt
@@ -2,20 +2,22 @@ package star.sky.voyager.hook.hooks.packageinstaller
 
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.Log
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object DisableCountCheck : HookRegister() {
     override fun init() = hasEnable("package_installer_remove_check") {
-        loadClass("com.miui.packageInstaller.model.RiskControlRules").methodFinder()
-            .filterByName("getCurrentLevel")
-            .first().createHook {
-                before { param ->
-                    Log.i("Hooked getCurrentLevel, param result = ${param.result}")
-                    param.result = 0
+        val riskControlRules =
+            loadClass("com.miui.packageInstaller.model.RiskControlRules")
+        try {
+            riskControlRules.methodFinder()
+                .filterByName("getCurrentLevel")
+                .first().createHook {
+                    returnConstant(0)
                 }
-            }
+        } catch (_: Throwable) {
+
+        }
     }
 }
