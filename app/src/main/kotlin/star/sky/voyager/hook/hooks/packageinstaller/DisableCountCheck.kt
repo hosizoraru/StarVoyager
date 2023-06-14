e72abecbf29ac89728f09a2f1b676e5984ffa80f
hosizoraru <hosizoraru@gmail.com>
2023-06-14 08:35:18 +0800

    优化 去除一些广告的hook

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/DisableCountCheck.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/DisableCountCheck.kt
@@ -1,6 +1,6 @@
 package star.sky.voyager.hook.hooks.packageinstaller
 
-import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
@@ -9,15 +9,11 @@ import star.sky.voyager.utils.key.hasEnable
 object DisableCountCheck : HookRegister() {
     override fun init() = hasEnable("package_installer_remove_check") {
         val riskControlRules =
-            loadClass("com.miui.packageInstaller.model.RiskControlRules")
-        try {
-            riskControlRules.methodFinder()
-                .filterByName("getCurrentLevel")
-                .first().createHook {
-                    returnConstant(0)
-                }
-        } catch (_: Throwable) {
-
-        }
+            loadClassOrNull("com.miui.packageInstaller.model.RiskControlRules")
+        riskControlRules?.methodFinder()
+            ?.filterByName("getCurrentLevel")
+            ?.first()?.createHook {
+                returnConstant(0)
+            }
     }
 }
--- app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/RemovePackageInstallerAds.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/packageinstaller/RemovePackageInstallerAds.kt
@@ -1,8 +1,8 @@
 package star.sky.voyager.hook.hooks.packageinstaller
 
-import android.content.Context
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.api.setBooleanField
 import star.sky.voyager.utils.init.HookRegister
@@ -11,36 +11,19 @@ import star.sky.voyager.utils.key.hasEnable
 object RemovePackageInstallerAds : HookRegister() {
     override fun init() = hasEnable("package_installer_remove_ads") {
         val miuiSettingsCompatClass =
-            loadClassOrNull("com.android.packageinstaller.compat.MiuiSettingsCompat")
-        val qaq = listOf("s", "q", "f", "t", "r")
+            loadClassOrNull("com.android.packageinstaller.compat.MiuiSettingsCompat")!!
 
-        miuiSettingsCompatClass?.methodFinder()
-            ?.filterByName("isPersonalizedAdEnabled")
-            ?.filterByReturnType(Boolean::class.java)
-            ?.firstOrNull()?.createHook {
-                after {
-                    it.result = false
+        try {
+            miuiSettingsCompatClass.methodFinder()
+                .filterByName("isPersonalizedAdEnabled")
+                .filterByReturnType(Boolean::class.java)
+                .toList().createHooks {
+                    before {
+                        it.result = false
+                    }
                 }
-            } ?: println("Could not find method 'isPersonalizedAdEnabled'")
+        } catch (_: Throwable) {
 
-        miuiSettingsCompatClass?.methodFinder()
-            ?.filterByName("isInstallRiskEnabled")
-            ?.filterByParamCount(1)
-            ?.filterByParamTypes(Context::class.java)
-            ?.firstOrNull()?.createHook {
-                after {
-                    it.result = false
-                }
-            } ?: println("Could not find method 'isInstallRiskEnabled'")
-
-        loadClassOrNull("m2.b")?.methodFinder()?.apply {
-            qaq.forEach { qaq ->
-                this
-                    .filterByName(qaq)
-                    .firstOrNull()?.createHook {
-                        returnConstant(false)
-                    } ?: println("Could not find method '$qaq' in 'm2.b'")
-            }
         }
 
         var letter = 'a'
@@ -49,14 +32,11 @@ object RemovePackageInstallerAds : HookRegister() {
                 val classIfExists =
                     loadClassOrNull("com.miui.packageInstaller.ui.listcomponets.${letter}0")
                 classIfExists?.let {
-                    it.methodFinder()
-                        .filterByName("a")
-                        .firstOrNull()?.createHook {
-                            after { hookParam ->
-                                hookParam.thisObject.setBooleanField("l", false)
-                            }
+                    it.methodFinder().filterByName("a").first().createHook {
+                        after { hookParam ->
+                            hookParam.thisObject.setBooleanField("l", false)
                         }
-                        ?: println("Could not find method 'a' in 'com.miui.packageInstaller.ui.listcomponets.${letter}0'")
+                    }
                 }
             } catch (t: Throwable) {
                 letter++
