30e6b6abb2f499c65638b932209b9b983b57b471
hosizoraru <hosizoraru@gmail.com>
2023-06-24 00:00:09 +0800

    修复一项判断写反了的问题

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/NotificationSettingsNoWhiteList.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/NotificationSettingsNoWhiteList.kt
@@ -8,7 +8,7 @@ import star.sky.voyager.utils.yife.Build.IS_INTERNATIONAL_BUILD
 
 object NotificationSettingsNoWhiteList : HookRegister() {
     override fun init() = hasEnable("notification_settings_no_white_list") {
-        if (!IS_INTERNATIONAL_BUILD) return@hasEnable
+        if (IS_INTERNATIONAL_BUILD) return@hasEnable
         setStaticObject(
             loadClass("com.android.systemui.statusbar.notification.NotificationSettingsManager"),
             "USE_WHITE_LISTS",
