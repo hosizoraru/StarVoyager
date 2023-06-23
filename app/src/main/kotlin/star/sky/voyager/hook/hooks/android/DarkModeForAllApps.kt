2b0e7ff7dc37a900f5cd85c73d835c971953afd0
hosizoraru <hosizoraru@gmail.com>
2023-06-24 02:59:00 +0800

    优化名单判断并且优化描述

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/android/DarkModeForAllApps.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/android/DarkModeForAllApps.kt
@@ -1,8 +1,10 @@
 package star.sky.voyager.hook.hooks.android
 
+import android.content.pm.ApplicationInfo
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
@@ -11,7 +13,10 @@ import star.sky.voyager.utils.yife.Build.IS_INTERNATIONAL_BUILD
 object DarkModeForAllApps : HookRegister() {
     override fun init() = hasEnable("dark_mode_for_all_apps") {
         if (IS_INTERNATIONAL_BUILD) return@hasEnable
-        loadClass("com.android.server.ForceDarkAppListManager").methodFinder()
+        val forceDarkAppListManager =
+            loadClass("com.android.server.ForceDarkAppListManager")
+
+        forceDarkAppListManager.methodFinder()
             .filterByName("getDarkModeAppList")
             .toList().createHooks {
                 before {
@@ -28,6 +33,19 @@ object DarkModeForAllApps : HookRegister() {
                         "IS_INTERNATIONAL_BUILD",
                         IS_INTERNATIONAL_BUILD
                     )
+                }
+            }
+
+        forceDarkAppListManager.methodFinder()
+            .filterByName("shouldShowInSettings")
+            .filterByParamTypes(ApplicationInfo::class.java)
+            .toList().createHooks {
+                before { param ->
+                    val info = param.args[0] as ApplicationInfo?
+                    param.result = !(info == null || (invokeMethodBestMatch(
+                        info,
+                        "isSystemApp"
+                    ) as Boolean) || (info.uid <= 10000))
                 }
             }
     }
