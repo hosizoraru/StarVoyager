c6bc612af1e004b515e5a51de4476d91f1679dae
hosizoraru <hosizoraru@gmail.com>
2023-07-22 12:55:23 +0800

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 26
-        versionName = "3.11"
+        versionName = "3.12"
         buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
     }
 
