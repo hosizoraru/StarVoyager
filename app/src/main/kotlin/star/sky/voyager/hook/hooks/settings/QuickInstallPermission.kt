27e9fa01a242f6a00a15b657fa49ff9244fe0f0f
hosizoraru <hosizoraru@gmail.com>
2023-08-11 15:17:58 +0800

    跳转优化

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/settings/QuickInstallPermission.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/settings/QuickInstallPermission.kt
@@ -1,29 +1,48 @@
 package star.sky.voyager.hook.hooks.settings
 
 import android.app.Activity
-import android.os.Bundle
+import android.provider.Settings
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
-import star.sky.voyager.utils.api.setObjectField
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object QuickInstallPermission : HookRegister() {
     override fun init() = hasEnable("system_settings_permission_unknown_origin_app") {
-        loadClass("com.android.settings.SettingsActivity").methodFinder()
-            .filterByName("redirectTabletActivity")
-            .filterByParamTypes(Bundle::class.java)
-            .toList().createHooks {
-                before { param ->
-                    val intent = (param.thisObject as Activity).intent
-                    if ("android.settings.MANAGE_UNKNOWN_APP_SOURCES" == intent.action && intent.data != null && "package" == intent.data!!.scheme) {
-                        param.thisObject.setObjectField(
-                            "initialFragmentName",
-                            "com.android.settings.applications.appinfo.ExternalSourcesDetails"
-                        )
-                    }
-                }
+        val settingsActivity =
+            loadClass("com.android.settings.SettingsActivity")
+        val redirectTabletActivity =
+            settingsActivity.methodFinder().filterByName("redirectTabletActivity").first()
+
+        redirectTabletActivity.createHook {
+            before {
+                val activity = it.thisObject as Activity
+                val intent = activity.intent
+                val action = intent.action
+                val data = intent.data
+                if (action != Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES || data == null || data.scheme != "package") return@before
+                activity.objectHelper().setObjectUntilSuperclass(
+                    "initialFragmentName",
+                    "com.android.settings.applications.appinfo.ExternalSourcesDetails"
+                )
             }
+        }
+
+        redirectTabletActivity.createHook {
+            before {
+                val activity = it.thisObject as Activity
+                val intent = activity.intent
+                val action = intent.action
+                val data = intent.data
+                if (action != Settings.ACTION_MANAGE_OVERLAY_PERMISSION || data == null || data.scheme != "package") return@before
+                activity.objectHelper()
+                    .setObjectUntilSuperclass(
+                        "initialFragmentName",
+                        "com.android.settings.applications.appinfo.DrawOverlayDetails"
+                    )
+            }
+        }
     }
 }
