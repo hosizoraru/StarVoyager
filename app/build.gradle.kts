9f116c9d838c260fd12f75a1e7422fc4d3ab613d
hosizoraru <hosizoraru@gmail.com>
2023-08-15 19:03:41 +0800

    3.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 27
-        versionName = "3.25"
+        versionName = "3.26"
         buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
     }
 
