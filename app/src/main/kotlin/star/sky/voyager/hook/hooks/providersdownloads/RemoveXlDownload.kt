888011845133bac665ab27ce9ec79d3b8d72f01b
hosizoraru <hosizoraru@gmail.com>
2023-08-17 04:12:48 +0800

    Fix RemoveXlDownload

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/providersdownloads/RemoveXlDownload.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/providersdownloads/RemoveXlDownload.kt
@@ -1,22 +1,28 @@
 package star.sky.voyager.hook.hooks.providersdownloads
 
-import android.os.Environment
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
-import java.io.File
-import java.io.FileNotFoundException
 
 object RemoveXlDownload : HookRegister() {
     override fun init() = hasEnable("remove_xl_download") {
-        val targetPath = File(Environment.getExternalStorageDirectory(), ".xlDownload").absoluteFile
-        File::class.java.methodFinder().filterByName("mkdirs").first().createHook {
-            before {
-                if ((it.thisObject as File).absoluteFile.equals(targetPath)) {
-                    it.throwable = FileNotFoundException("blocked")
-                }
-            }
+//        val targetPath = File(Environment.getExternalStorageDirectory(), ".xlDownload").absoluteFile
+//        File::class.java.methodFinder().filterByName("mkdirs").first().createHook {
+//            before {
+//                if ((it.thisObject as File).absoluteFile.equals(targetPath)) {
+//                    it.throwable = FileNotFoundException("blocked")
+//                }
+//            }
+//        }
+        val clazzXLConfig =
+            loadClass("com.android.providers.downloads.config.XLConfig")
+
+        clazzXLConfig.methodFinder().filter {
+            name in setOf("setDebug", "setSoDebug")
+        }.toList().createHooks {
+            returnConstant(null)
         }
     }
 }
