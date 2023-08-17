2e67303a266c67d433e4223b552e5966597036f6
hosizoraru <hosizoraru@gmail.com>
2023-08-17 09:55:24 +0800

    解锁实时字幕

--- app/src/main/kotlin/star/sky/voyager/hook/apps/AiasstVision.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/apps/AiasstVision.kt
@@ -1,6 +1,7 @@
 package star.sky.voyager.hook.apps
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import star.sky.voyager.hook.hooks.aiasstvision.AiSubtitles
 import star.sky.voyager.hook.hooks.aiasstvision.DesktopModeScreenTranslate
 import star.sky.voyager.hook.hooks.aiasstvision.MeetingMode
 import star.sky.voyager.hook.hooks.aiasstvision.SplitScreenTranslate
@@ -15,6 +16,7 @@ object AiasstVision : AppRegister() {
             MeetingMode, // 解锁会议模式
             DesktopModeScreenTranslate, // 允许在工作台模式下使用屏幕翻译
             SplitScreenTranslate, // 解锁分屏翻译
+            AiSubtitles, // 实时字幕
         )
     }
 }
