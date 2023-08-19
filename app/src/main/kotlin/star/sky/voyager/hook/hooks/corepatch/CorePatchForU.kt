2bfae2c8f5a44855ac60861f1633e6fe314159fc
hosizoraru <hosizoraru@gmail.com>
2023-08-19 14:10:19 +0800

    fix android U corepatch

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/corepatch/CorePatchForU.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/corepatch/CorePatchForU.kt
@@ -1,8 +1,10 @@
 package star.sky.voyager.hook.hooks.corepatch
 
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import java.lang.reflect.InvocationTargetException
+
 
 open class CorePatchForU : CorePatchForT() {
     @Throws(
@@ -19,6 +21,33 @@ open class CorePatchForU : CorePatchForT() {
             "android.content.pm.PackageInfoLite",
             ReturnConstant(prefs, "down_grade", null)
         )
+
+        if (prefs.getBoolean(
+                "digestCreak",
+                true
+            ) && prefs.getBoolean("UsePreSig", false)
+        ) {
+            findAndHookMethod("com.android.server.pm.InstallPackageHelper",
+                loadPackageParam.classLoader,
+                "doesSignatureMatchForPermissions",
+                String::class.java,
+                "com.android.server.pm.parsing.pkg.ParsedPackage",
+                Int::class.javaPrimitiveType,
+                object : XC_MethodHook() {
+                    @Throws(Throwable::class)
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        // If we decide to crack this then at least make sure they are same apks, avoid another one that tries to impersonate.
+                        if (param.result == false) {
+                            val pPname =
+                                XposedHelpers.callMethod(param.args[1], "getPackageName") as String
+                            if (pPname.contentEquals(param.args[0] as String)) {
+                                param.result = true
+                            }
+                        }
+                    }
+                })
+        }
+
         findAndHookMethod("com.android.server.pm.ScanPackageUtils",
             loadPackageParam.classLoader,
             "assertMinSignatureSchemeIsValid",
