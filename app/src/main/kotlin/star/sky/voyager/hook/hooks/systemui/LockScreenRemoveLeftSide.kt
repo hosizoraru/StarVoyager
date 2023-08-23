65e4e431cd41b213b4ce411dc56c07f0f04ece33
hosizoraru <hosizoraru@gmail.com>
2023-08-24 06:11:11 +0800

    优化hook思路

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/LockScreenRemoveLeftSide.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/systemui/LockScreenRemoveLeftSide.kt
@@ -8,12 +8,17 @@ import star.sky.voyager.utils.key.hasEnable
 
 object LockScreenRemoveLeftSide : HookRegister() {
     override fun init() = hasEnable("remove_the_left_side_of_the_lock_screen") {
-        loadClass("com.android.keyguard.negative.MiuiKeyguardMoveLeftViewContainer").methodFinder()
-            .filterByName("inflateLeftView")
+//        loadClass("com.android.keyguard.negative.MiuiKeyguardMoveLeftViewContainer").methodFinder()
+//            .filterByName("inflateLeftView")
+//            .first().createHook {
+//                before {
+//                    it.result = null
+//                }
+//            }
+        loadClass("com.android.keyguard.negative.MiuiKeyguardMoveLeftViewContainer")
+            .methodFinder().filterByName("isSupportRightMove")
             .first().createHook {
-                before {
-                    it.result = null
-                }
+                returnConstant(false)
             }
     }
 }
