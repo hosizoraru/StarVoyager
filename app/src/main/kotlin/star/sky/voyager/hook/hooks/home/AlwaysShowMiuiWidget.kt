6f2bf51c4d97b46cc5beba91d37142c1bcedb1c2
hosizoraru <hosizoraru@gmail.com>
2023-09-02 13:34:45 +0800

    fix

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/AlwaysShowMiuiWidget.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/AlwaysShowMiuiWidget.kt
@@ -3,52 +3,65 @@ package star.sky.voyager.hook.hooks.home
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
-import de.robv.android.xposed.XC_MethodHook
-import star.sky.voyager.utils.api.setObjectField
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object AlwaysShowMiuiWidget : HookRegister() {
     override fun init() = hasEnable("Show_MIUI_Widget") {
-        var hook1: XC_MethodHook.Unhook? = null
-        var hook2: XC_MethodHook.Unhook? = null
-        val classNames = listOf(
-            "com.miui.home.launcher.widget.WidgetsVerticalAdapter",
-            "com.miui.home.launcher.widget.BaseWidgetsVerticalAdapter"
-        )
-
-        classNames.forEach { className ->
-            val result = runCatching {
-                loadClass(className).methodFinder()
-                    .filterByName("buildAppWidgetsItems")
-                    .first()
-                    .createHook {
-                        before {
-                            hook1 =
-                                loadClass("com.miui.home.launcher.widget.MIUIAppWidgetInfo").methodFinder()
-                                    .filterByName("initMiuiAttribute")
-                                    .filterByParamCount(1)
-                                    .first().createHook {
-                                        after {
-                                            it.thisObject.setObjectField("isMIUIWidget", false)
-                                        }
-                                    }
-                            hook2 =
-                                loadClass("com.miui.home.launcher.MIUIWidgetUtil").methodFinder()
-                                    .filterByName("isMIUIWidgetSupport")
-                                    .first().createHook {
-                                        after {
-                                            it.result = false
-                                        }
-                                    }
-                        }
-                        after {
-                            hook1?.unhook()
-                            hook2?.unhook()
-                        }
+        loadClass("com.miui.home.launcher.MIUIWidgetUtil").methodFinder()
+            .filterByName("isMIUIWidgetSupport")
+            .first().createHook {
+                after { param ->
+                    if (Throwable().stackTrace.any {
+                            it.className in setOf(
+                                "com.miui.home.launcher.widget.WidgetsVerticalAdapter",
+                                "com.miui.home.launcher.widget.BaseWidgetsVerticalAdapter"
+                            )
+                        }) {
+                        param.result = false
                     }
+                }
             }
-            if (result.isSuccess) return@forEach
-        }
     }
 }
+
+//var hook1: XC_MethodHook.Unhook? = null
+//var hook2: XC_MethodHook.Unhook? = null
+//val classNames = listOf(
+//    "com.miui.home.launcher.widget.WidgetsVerticalAdapter",
+//    "com.miui.home.launcher.widget.BaseWidgetsVerticalAdapter"
+//)
+//
+//classNames.forEach { className ->
+//    val result = runCatching {
+//        loadClass(className).methodFinder()
+//            .filterByName("buildAppWidgetsItems")
+//            .first()
+//            .createHook {
+//                before {
+//                    hook1 =
+//                        loadClass("com.miui.home.launcher.widget.MIUIAppWidgetInfo").methodFinder()
+//                            .filterByName("initMiuiAttribute")
+//                            .filterByParamCount(1)
+//                            .first().createHook {
+//                                after {
+//                                    it.thisObject.setObjectField("isMIUIWidget", false)
+//                                }
+//                            }
+//                    hook2 =
+//                        loadClass("com.miui.home.launcher.MIUIWidgetUtil").methodFinder()
+//                            .filterByName("isMIUIWidgetSupport")
+//                            .first().createHook {
+//                                after {
+//                                    it.result = false
+//                                }
+//                            }
+//                }
+//                after {
+//                    hook1?.unhook()
+//                    hook2?.unhook()
+//                }
+//            }
+//    }
+//    if (result.isSuccess) return@forEach
+//}
--- app/src/main/kotlin/star/sky/voyager/hook/hooks/home/ResizableWidgets.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/home/ResizableWidgets.kt
@@ -1,29 +1,45 @@
 package star.sky.voyager.hook.hooks.home
 
 import android.appwidget.AppWidgetProviderInfo
+import com.github.kyuubiran.ezxhelper.ClassLoaderProvider.classLoader
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
+import star.sky.voyager.utils.yife.DexKit.dexKitBridge
 
 object ResizableWidgets : HookRegister() {
     override fun init() = hasEnable("resizable_widgets") {
-        loadClass("android.appwidget.AppWidgetHostView", null).methodFinder()
-            .filterByName("getAppWidgetInfo")
-            .toList().createHooks {
-                after { param ->
-                    val widgetInfo = param.result as AppWidgetProviderInfo
-//                    Log.i("before: " + widgetInfo.toString() + " " + widgetInfo.minHeight + " " + widgetInfo.minWidth + " " + widgetInfo.minResizeHeight + " " + widgetInfo.minResizeWidth)
-                    widgetInfo.resizeMode =
-                        AppWidgetProviderInfo.RESIZE_VERTICAL or AppWidgetProviderInfo.RESIZE_HORIZONTAL
-                    widgetInfo.minHeight = 0
-                    widgetInfo.minWidth = 0
-                    widgetInfo.minResizeHeight = 0
-                    widgetInfo.minResizeWidth = 0
-                    param.result = widgetInfo
-//                    Log.i("after: " + widgetInfo.toString() + " " + widgetInfo.minHeight + " " + widgetInfo.minWidth + " " + widgetInfo.minResizeHeight + " " + widgetInfo.minResizeWidth)
+        val qwq1 =
+            loadClass("android.appwidget.AppWidgetHostView", null).methodFinder()
+                .filterByName("getAppWidgetInfo").toList()
+        val qwq2 =
+            dexKitBridge.findMethod {
+                methodName = "getAppWidgetInfo"
+//                methodReturnType = "android.appwidget.AppWidgetProviderInfo"
+            }.map { it.getMethodInstance(classLoader) }.toList()
+
+        setOf(qwq1, qwq2).forEach { methods1 ->
+            methods1.filter {
+                it.returnType == AppWidgetProviderInfo::class.java
+            }.forEach { methods2 ->
+                methods2.createHook {
+                    after { param ->
+                        val widgetInfo =
+                            param.result as AppWidgetProviderInfo
+                        widgetInfo.resizeMode =
+                            AppWidgetProviderInfo.RESIZE_VERTICAL or AppWidgetProviderInfo.RESIZE_HORIZONTAL
+                        widgetInfo.minHeight = 0
+                        widgetInfo.minWidth = 0
+                        widgetInfo.minResizeHeight = 0
+                        widgetInfo.minResizeWidth = 0
+                        param.result = widgetInfo
+                    }
                 }
             }
+        }
     }
 }
+//                    Log.i("before: " + widgetInfo.toString() + " " + widgetInfo.minHeight + " " + widgetInfo.minWidth + " " + widgetInfo.minResizeHeight + " " + widgetInfo.minResizeWidth)
+//                    Log.i("after: " + widgetInfo.toString() + " " + widgetInfo.minHeight + " " + widgetInfo.minWidth + " " + widgetInfo.minResizeHeight + " " + widgetInfo.minResizeWidth)
