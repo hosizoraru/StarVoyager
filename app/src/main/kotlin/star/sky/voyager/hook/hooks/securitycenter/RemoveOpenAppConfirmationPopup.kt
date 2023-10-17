fc22038dc46f5e512d05fe3a2ea1842db8d1773f
hosizoraru <hosizoraru@gmail.com>
2023-10-17 20:06:02 +0800

    dexkit 2.0.0-rc7

--- app/src/main/kotlin/star/sky/voyager/hook/hooks/securitycenter/RemoveOpenAppConfirmationPopup.kt
+++ app/src/main/kotlin/star/sky/voyager/hook/hooks/securitycenter/RemoveOpenAppConfirmationPopup.kt
@@ -1,5 +1,6 @@
 package star.sky.voyager.hook.hooks.securitycenter
 
+import android.annotation.SuppressLint
 import android.widget.TextView
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
@@ -8,6 +9,7 @@ import star.sky.voyager.utils.init.HookRegister
 import star.sky.voyager.utils.key.hasEnable
 
 object RemoveOpenAppConfirmationPopup : HookRegister() {
+    @SuppressLint("DiscouragedApi")
     override fun init() = hasEnable("remove_open_app_confirmation_popup") {
         loadClass("android.widget.TextView").methodFinder()
             .filterByName("setText")
